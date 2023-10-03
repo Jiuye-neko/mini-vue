06e1454e60a07f01b97a303e8508a69b7d97d887
Jiuye_ <49723697+Jiuye-neko@users.noreply.github.com>
2023-10-03 12:25:43 +0000

    feat: 实现 shallowReadonly

---
+++ tests/shallowReadonly.spec.ts
@@ -0,0 +1,18 @@
+import { isReadonly, shallowReadonly } from "../src/reactive";
+
+describe('shallowReadonly', () => {
+ it('readonly is shallow', () => {
+  const val = shallowReadonly({ foo: 1, bar: { baz: 2}});
+
+  expect(isReadonly(val)).toBe(true);
+  expect(isReadonly(val.bar)).toBe(false);
+ })
+
+ it('shallowReadonly should not be set', () => {
+  const val = shallowReadonly({foo: 1});
+  console.warn = jest.fn();
+  val.foo++;
+
+  expect(console.warn).toHaveBeenCalled();
+ })
+})
