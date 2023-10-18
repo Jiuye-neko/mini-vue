c543fc7b157c0e8619b1e894c071871ddce7a3dd
Jiuye-neko <740359023@qq.com>
2023-10-18 10:23:14 +0800

    feat: 实现 element 主流程

--- examples/App.js
+++ examples/App.js
@@ -1,8 +1,17 @@
-import { h } from '../lib/guide-mini-vue.esm';
+import { h } from '../lib/guide-mini-vue.esm.js';
 
 export const App = {
   render() {
-    return h('div', 'hello, ' + this.msg);
+    return h(
+      'div',
+      { class: ['center', 'yellow'] },
+      // 'hello, ' + this.msg
+      [
+        // 'hey there',
+        h('p', { class: 'blue' }, 'hello'),
+        h('p', { class: 'red' }, 'mini-vue'),
+      ]
+    );
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from '../shared/index';
 import { createComponentInstance, setupComponent } from './component';
 
 export function render(vnode, container) {
@@ -7,10 +8,42 @@ export function render(vnode, container) {
 
 function patch(vnode: any, container: any) {
   // 判断当前的 vnode 是 Component 还是 Element 类型
-  // Component
-  // 开始处理 Component
-  processComponent(vnode, container);
-  // TODO Element
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    // Component
+    // 开始处理 Component
+    processComponent(vnode, container);
+  }
+}
+
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type);
+
+  const { children } = vnode;
+  if (typeof children === 'string') {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+
+  const { props } = vnode;
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  container.append(el);
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
 }
 
 function processComponent(vnode: any, container: any) {
